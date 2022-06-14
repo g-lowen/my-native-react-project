0dadff023cda3d29fe45942a68aaec1360fbf116
Gustav Löwen-Åberg <gustav_lowen@hotmail.com>
2022-06-14 14:30:45 +0200

    Made code accessable for Android

--- App.js
+++ App.js
@@ -1,57 +1,61 @@
-import * as React from 'react'
+import * as React from "react";
 // import { StatusBar } from 'expo-status-bar'
 import {
   StyleSheet,
-  Text,
+  // Text,
   View,
   Button,
   TextInput,
   Image,
   ImageBackground,
-  ScrollView
-} from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { DataTable } from 'react-native-paper'
+  ScrollView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { DataTable } from "react-native-paper";
+import backgroundImg from "./assets/background-dice.jpg";
+import logoImg from "./assets/dice-logo-sm.png";
+import PropTypes from "prop-types";
 
-function HomeScreen({ navigation, route }) {
+const Stack = createNativeStackNavigator();
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+FetchScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+
+function HomeScreen({ navigation }) {
   const [name, setName] = React.useState(null),
-    [players, setPlayers] = React.useState([])
+    [players, setPlayers] = React.useState([]);
 
   const playerTable = players.map((player, index) => (
     <DataTable style={styles.table} key={index}>
-      <DataTable.Header style={styles.tableHeader}>
-        <DataTable.Title>{player.name}</DataTable.Title>
+      <DataTable.Header style={styles.tableHeaderContainer}>
+        <DataTable.Title style={styles.tableHeader}>
+          {player.name}
+        </DataTable.Title>
       </DataTable.Header>
       {player.values.map((value, index) => (
         <DataTable.Row key={index}>
           <DataTable.Cell style={styles.indexCell}>{index + 1}</DataTable.Cell>
           <DataTable.Cell style={styles.indexCell2}>
-            {/* <TextInput
-              style={styles.valueInput}
-              keyboardType="numeric"
-              maxLength={5}
-              onChangeText={(value) => {
-                // console.log('Input value: ', value)
-                // console.log('Input index: ', index)
-                player.values[index] = value
-                console.log('Players före: ', players)
-                setPlayers(players)
-                console.log('Players efter: ', players)
-              }}
-            /> */}
-            <TextInput
-              style={styles.valueInput}
-              keyboardType="numeric"
-              maxLength={5}
-              onBlur={(event) => {
-                player.values[index] = event.target.value
-                console.log('Players före: ', players)
-                setPlayers(players)
-                console.log('Players efter: ', players)
-              }}
-            />
+            <View style={{ flex: 1 }}>
+              <TextInput
+                style={styles.valueInput}
+                keyboardType="numeric"
+                maxLength={5}
+                onBlur={(event) => {
+                  player.values[index] = event.target.value;
+                  console.log("Players före: ", players);
+                  setPlayers(players);
+                  // setPlayers([...players, player]);
+                  console.log("Players efter: ", players);
+                }}
+              />
+            </View>
           </DataTable.Cell>
         </DataTable.Row>
       ))}
@@ -60,23 +64,23 @@ function HomeScreen({ navigation, route }) {
         <DataTable.Cell style={styles.indexCell2}>{player.sum}</DataTable.Cell>
       </DataTable.Row>
     </DataTable>
-  ))
+  ));
 
   function onPressHandler() {
     const player = {
       name: name,
       values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      sum: 0
-    }
-    setPlayers([...players, player])
-    console.log('onPressHandler players: ', players)
+      sum: 0,
+    };
+    setPlayers([...players, player]);
+    // console.log("onPressHandler players: ", players);
   }
 
   React.useEffect(() => {
     // console.log('useEffect körs!')
     navigation.setOptions({
       headerRight: () => (
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: "row" }}>
           <TextInput
             style={styles.input}
             placeholder="Player name"
@@ -84,97 +88,97 @@ function HomeScreen({ navigation, route }) {
           />
           <Button onPress={onPressHandler} title="Add player" color="#20232a" />
         </View>
-      )
-    })
+      ),
+    });
     players.forEach((player) => {
-      const strToNum = player.values.map((str) => Number(str))
-      const initialValue = 0
+      const strToNum = player.values.map((str) => Number(str));
+      const initialValue = 0;
       const sum = strToNum.reduce(
         (previousValue, currentValue) => previousValue + currentValue,
         initialValue
-      )
-      player.sum = sum
-      setPlayers(JSON.parse(JSON.stringify(players)))
-      console.log(players)
-    })
-    console.log('useEffect players: ', players)
-  }, [navigation, name])
+      );
+      player.sum = sum;
+      setPlayers(JSON.parse(JSON.stringify(players)));
+      console.log(players);
+    });
+    console.log("useEffect players: ", players);
+  }, [navigation, name]);
 
   React.useEffect(() => {
-    console.log('useEffect körs!')
+    console.log("useEffect körs!");
     players.forEach((player) => {
-      const strToNum = player.values.map((str) => Number(str))
-      const initialValue = 0
+      const strToNum = player.values.map((str) => Number(str));
+      const initialValue = 0;
       const sum = strToNum.reduce(
         (previousValue, currentValue) => previousValue + currentValue,
         initialValue
-      )
-      player.sum = sum
-      setPlayers(JSON.parse(JSON.stringify(players)))
-      console.log(players)
-    })
-    console.log('useEffect players: ', players)
-  }, [players.values])
+      );
+      player.sum = sum;
+      setPlayers(JSON.parse(JSON.stringify(players)));
+      console.log(players);
+    });
+    console.log("useEffect players: ", players);
+  }, [players.values]);
 
   return (
     <ImageBackground
-      source={require('./assets/background-dice.jpg')}
+      source={backgroundImg}
       resizeMode="cover"
       style={styles.backgroundImage}
     >
       <LinearGradient
-        colors={['rgba(247, 253, 254, 0.756)', 'rgba(242, 255, 255, 0.688)']}
+        colors={["rgba(247, 253, 254, 0.756)", "rgba(242, 255, 255, 0.688)"]}
         style={styles.container}
       >
         <ScrollView>{playerTable}</ScrollView>
         <Button
           color="#20232a"
           title="Go fetch"
-          onPress={() => navigation.navigate('Fetch')}
+          onPress={() => navigation.navigate("Fetch")}
         />
       </LinearGradient>
     </ImageBackground>
-  )
+  );
 }
 
 function FetchScreen({ navigation }) {
-  const [dog, setDog] = React.useState('')
-  const [dogCycle, setDogCycle] = React.useState('')
+  const [dog, setDog] = React.useState(null);
+  const [dogCycle, setDogCycle] = React.useState("");
   function fetchData() {
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => response.json())
       .then((result) => {
-        setDog(result.message)
-      })
+        setDog(result.message);
+      });
   }
 
-  React.useEffect(fetchData, [dogCycle])
+  React.useEffect(fetchData, [dogCycle]);
 
   return (
     <ImageBackground
-      source={dog}
+      source={{ uri: dog }}
       resizeMode="cover"
       style={styles.backgroundImage}
     >
       <LinearGradient
-        colors={['rgba(247, 253, 254, 0.756)', 'rgba(242, 255, 255, 0.688)']}
+        colors={["rgba(247, 253, 254, 0.756)", "rgba(242, 255, 255, 0.688)"]}
         style={styles.container}
       >
         <Button
           color="#282c34"
           title="Fetch another dog"
           onPress={() => {
-            if (dogCycle === 'cycle') {
-              setDogCycle('cycle again')
+            if (dogCycle === "cycle") {
+              setDogCycle("cycle again");
             } else {
-              setDogCycle('cycle')
+              setDogCycle("cycle");
             }
           }}
         />
         <View
-          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          <Image style={{ width: 100, height: 100 }} source={dog} />
+          <Image style={{ width: 100, height: 100 }} source={{ uri: dog }} />
         </View>
         <Button
           color="#20232a"
@@ -183,19 +187,12 @@ function FetchScreen({ navigation }) {
         />
       </LinearGradient>
     </ImageBackground>
-  )
+  );
 }
 
 function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={require('./assets/dice-logo-sm.png')}
-    />
-  )
+  return <Image style={{ width: 50, height: 50 }} source={logoImg} />;
 }
-
-const Stack = createNativeStackNavigator()
 
 function App() {
   return (
@@ -205,15 +202,15 @@ function App() {
           name="Home"
           component={HomeScreen}
           options={{
-            title: 'My home',
+            title: "My home",
             headerStyle: {
-              backgroundColor: '#20232a'
+              backgroundColor: "#20232a",
             },
-            headerTintColor: '#fff',
+            headerTintColor: "#fff",
             headerTitleStyle: {
-              fontWeight: 'bold'
+              fontWeight: "bold",
             },
-            headerTitle: (props) => <LogoTitle {...props} />
+            headerTitle: (props) => <LogoTitle {...props} />,
           }}
           style={styles.test}
         />
@@ -221,66 +218,78 @@ function App() {
           name="Fetch"
           component={FetchScreen}
           options={{
-            title: 'Go fetch!',
+            title: "Go fetch!",
             headerStyle: {
-              backgroundColor: '#20232a',
+              backgroundColor: "#20232a",
               elevation: 0,
               shadowOpacity: 0,
-              borderBottomWidth: 0
+              borderBottomWidth: 0,
             },
-            headerTintColor: '#fff',
+            headerTintColor: "#fff",
             headerTitleStyle: {
-              fontWeight: 'bold'
+              fontWeight: "bold",
             },
-            headerTitle: (props) => <LogoTitle {...props} />
+            headerTitle: (props) => <LogoTitle {...props} />,
           }}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   test: {
-    borderBottomColor: 'red'
+    borderBottomColor: "red",
   },
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   backgroundImage: {
-    height: '100%',
-    width: '100%',
-    flex: 1
+    height: "100%",
+    width: "100%",
+    flex: 1,
   },
   table: {
     margin: 15,
     padding: 15,
-    backgroundColor: 'rgba(1, 1, 1, 0)',
-    width: '90%'
+    backgroundColor: "rgba(1, 1, 1, 0)",
+    width: "90%",
+  },
+  tableHeaderContainer: {
+    backgroundColor: "#DCDCDC",
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   tableHeader: {
-    backgroundColor: '#DCDCDC'
+    // backgroundColor: "green",
+    flex: 1,
+    alignItems: "center",
+    color: "#fff",
   },
   input: {
-    color: '#fff'
+    color: "#fff",
     // borderColor: '#fff',
     // borderWidth: 1
   },
   valueInput: {
     // paddingLeft: 35,
-    paddingTop: 15,
-    paddingRight: 70,
-    paddingBottom: 15
-    // backgroundColor: 'red'
+    // paddingTop: 15,
+    // paddingRight: 70,
+    // paddingBottom: 15,
+    alignItems: "stretch",
+    flex: 1,
+    backgroundColor: "red",
+    width: 100,
   },
   indexCell: {
-    // backgroundColor: 'green',
-    flex: 0.2
+    backgroundColor: "green",
+    flex: 0.2,
   },
   indexCell2: {
-    // backgroundColor: 'blue'
-  }
-})
+    backgroundColor: "blue",
+  },
+});
 
-export default App
+export default App;
--- TODO.md
+++ TODO.md
@@ -4,21 +4,21 @@ För G:
 
 ✓ Skapa ett React Native-projekt (eller liknande, till exempel får Expo användas) som använder fetch eller liknande för att hämta (och rendera) JSON-data från en webbtjänst (Det är tillåtet att använda ett eget backend)
 
-- Använd åtminstone ett React Native-bibliotek eller liknande\*
-- Använd åtminstone fem Core Components and APIs-komponenter eller liknande\*
-- node_modules, samt filer som matchas av .gitignore, ska inte ingå i inlämningen
-- Inlämningens omfattning måste vara tillräckligt stor (Det måste alltså ingå tillräckligt mycket kod, och kod med tillräckligt hög komplexitet och kvalitet)
+✓ Använd åtminstone ett React Native-bibliotek eller liknande\*
+✓ Använd åtminstone fem Core Components and APIs-komponenter eller liknande\*
+✓ node_modules, samt filer som matchas av .gitignore, ska inte ingå i inlämningen
+✓ Inlämningens omfattning måste vara tillräckligt stor (Det måste alltså ingå tillräckligt mycket kod, och kod med tillräckligt hög komplexitet och kvalitet)
   ✓ Delta på det obligatoriska avstämningsmötet på plats den 14/6
   ✓ Presentera appen på Android och/eller iOS, och dess kod, under 5-10 minuter (exklusive frågor) på plats och på svenska den 17/6 (Presentationen behöver inte innehålla slides)
 
 För VG så ska, utöver att kraven för G är uppfyllda, följande vara uppfyllt:
 
-- Skapa en egen splash screen
+✓ Skapa en egen splash screen
 - Använd åtminstone två ytterligare (alltså tre sammanlagt) React Native-bibliotek eller liknande\*
 - Använd åtminstone tio Core Components and APIs-komponenter, inklusive de fem komponenter som ingick bland G-kraven, samt inklusive antingen en - Android-specifik komponent eller en iOS-specifik komponent (eller båda)\*
-- Använd React Navigation (som rekommenderas av Expo-projektet), React Router, eller liknande, för routing
-- Lämna in helt konsistent formaterad kod (Förslagsvis kod formaterad automatiskt via Format On Save och Prettier)
-- Följ kodkonventioner och best practices i större utsträckning än för G
+✓ Använd React Navigation (som rekommenderas av Expo-projektet), React Router, eller liknande, för routing
+✓ Lämna in helt konsistent formaterad kod (Förslagsvis kod formaterad automatiskt via Format On Save och Prettier)
+✓ Följ kodkonventioner och best practices i större utsträckning än för G
 
 Biblioteks- och komponentanvändningen måste inte "passa in" eller vara meningsfull, men nyttjandet måste ha någon sorts effekt i appen. Biblioteken kan vara kompatibla med Expo:s “Managed workflow” (de måste alltså inte kräva till Expo extern “native”-funktionalitet, eller ett “Bare workflow”). Routing-biblioteket räknas inte som ett bibliotek i detta sammanhang.
 
